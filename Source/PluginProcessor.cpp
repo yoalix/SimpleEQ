1ca0d75346ec1e1337af43ce1309809a23b4a069
Yoali <yoali.carachure@gmail.com>
2023-12-26 13:36:35 -0600

    refactor making coefficients

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -234,10 +234,7 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements) {
 
 void SimpleEqAudioProcessor::updateLowCutFilters(
     const ChainSettings& chainSettings) {
-  auto cutCoefficients = juce::dsp::FilterDesign<float>::
-      designIIRHighpassHighOrderButterworthMethod(
-          chainSettings.lowCutFreq, getSampleRate(),
-          2 * (chainSettings.lowCutSlope + 1));
+  auto cutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
 
   auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
   auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
@@ -248,10 +245,7 @@ void SimpleEqAudioProcessor::updateLowCutFilters(
 
 void SimpleEqAudioProcessor::updateHighCutFilters(
     const ChainSettings& chainSettings) {
-  auto highCutCoefficients = juce::dsp::FilterDesign<float>::
-      designIIRLowpassHighOrderButterworthMethod(
-          chainSettings.highCutFreq, getSampleRate(),
-          2 * (chainSettings.highCutSlope + 1));
+  auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
 
   auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
   auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
