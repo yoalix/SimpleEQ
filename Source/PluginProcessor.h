9acd3f82be09c5498f803fced602f99682c73423
Yoali <yoali.carachure@gmail.com>
2023-12-25 21:05:05 -0600

    refactor low freq filter

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -92,6 +92,42 @@ private:
   void updatePeakFilter(const ChainSettings &chainSettings);
   using Coefficients = Filter::CoefficientsPtr;
   static void updateCoefficients(Coefficients &old, const Coefficients &replacments);
+
+  template <int Index, typename ChainType, typename CoefficientType>
+  void update(ChainType &chain, const CoefficientType &coefficients)
+  {
+    updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+    chain.template setBypassed<Index>(false);
+  }
+
+  template <typename ChainType, typename CoefficientType>
+  void updateCutFilter(ChainType &chain, const CoefficientType &coefficients, const Slope &slope)
+  {
+    chain.template setBypassed<0>(true);
+    chain.template setBypassed<1>(true);
+    chain.template setBypassed<2>(true);
+    chain.template setBypassed<3>(true);
+
+    switch (slope)
+    {
+    case Slope_48:
+    {
+      update<3>(chain, coefficients);
+    }
+    case Slope_36:
+    {
+      update<2>(chain, coefficients);
+    }
+    case Slope_24:
+    {
+      update<1>(chain, coefficients);
+    }
+    case Slope_12:
+    {
+      update<0>(chain, coefficients);
+    }
+    }
+  }
   //==============================================================================
   JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(SimpleEqAudioProcessor)
 };
